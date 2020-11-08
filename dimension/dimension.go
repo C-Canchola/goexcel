d5d0fcc95c2312f9981fd30794c7ffdef1f2af17
cancholacaleb <35677653+cancholacaleb@users.noreply.github.com>
2020-11-08 11:54:34 -0700

    added dimension tests using an excel data file

--- dimension/dimension.go
+++ dimension/dimension.go
@@ -11,16 +11,29 @@ import (
 // TableDimension provides information about a tab's
 // tabular dimensions.
 // These being properties such as row count and column count.
+// RowCount is the number of non-empty rows not including the header.
 type TableDimension struct {
 	RowCount, ColumnCount int
 }
-
+// ErrNoHeaderRow occurs when a dimension is made on a tab
+// with now values in the first row.
 var ErrNoHeaderRow = errors.New("dimension: no header row on tab")
 
-//TODO Function which takes an excelize file and sheet name
-// and returns a TableDimension
+// MakeTableDimension make a TableDimension struct from
+// an excelize file and sheet name.
 func MakeTableDimension(f *excelize.File, sheet string)(TableDimension, error){
-
+	cells, err := f.GetRows(sheet)
+	if err != nil{
+		return TableDimension{},err
+	}
+	colCount := getColumnCount(cells)
+	if colCount == 0{
+		return TableDimension{}, ErrNoHeaderRow
+	}
+	return TableDimension{
+		RowCount:    getNonEmptyRowCount(cells, colCount),
+		ColumnCount: colCount,
+	}, nil
 }
 
 func getColumnCount(cells [][]string)int{
@@ -31,7 +44,37 @@ func getColumnCount(cells [][]string)int{
 	cnt := 0
 	for firstRow[cnt] != ""{
 		cnt++
+
+		if cnt == len(firstRow){
+			return cnt
+		}
 	}
 	return cnt
 }
 
+// getNonEmptyRowCount returns the number of consecutive non empty rows
+// not including the header row.
+// Includes column count in order to not include rows
+// of data that may have empty values under the column
+// subset but non empty values outside.
+func getNonEmptyRowCount(cells [][]string, colCount int)int{
+	if colCount == 0{
+		return 0
+	}
+	nonEmptyRowCount := 0
+	for i := 1; i < len(cells); i++{
+		if rowEmpty(cells[i], colCount){
+			return nonEmptyRowCount
+		}
+		nonEmptyRowCount++
+	}
+	return nonEmptyRowCount
+}
+func rowEmpty(row []string, colCount int)bool{
+	for i := 0; i < colCount;i++{
+		if row[i] != ""{
+			return false
+		}
+	}
+	return true
+}
---
+++ dimension/dimension_test.go
@@ -0,0 +1,78 @@
+package dimension
+
+import (
+	"github.com/360EntSecGroup-Skylar/excelize/v2"
+	"path/filepath"
+	"testing"
+)
+
+var dataFilePath = filepath.Join("data", "data.xlsx")
+const twoRowFourColumnName = "TWO_ROW_FOUR_COLUMN"
+const zeroRowFourColumnName = "ZERO_ROW_FOUR_COLUMN"
+const blankName = "BLANK"
+
+func getDataFile()(*excelize.File, error){
+	return excelize.OpenFile(dataFilePath)
+}
+
+func getTabCells(tabName string)([][]string, error){
+	f, err := getDataFile()
+	if err != nil{
+		return nil, err
+	}
+	return f.GetRows(tabName)
+}
+
+func TestDimensionGetters(t *testing.T){
+	cells, err := getTabCells(twoRowFourColumnName)
+	if err != nil{
+		t.Fatal(err)
+	}
+
+	colCount := getColumnCount(cells)
+	if colCount != 4{
+		t.Error("colCount should equal 4 but equals", colCount)
+	}
+
+	rowCount := getNonEmptyRowCount(cells, colCount)
+	if rowCount != 2{
+		t.Error("rowCount should be equal to 2 but equals", rowCount)
+	}
+
+}
+
+func TestMakeTableDimension(t *testing.T) {
+	f, err := getDataFile()
+	if err != nil{
+		t.Fatal(err)
+	}
+	dimension, err := MakeTableDimension(f, twoRowFourColumnName)
+	if err != nil{
+		t.Fatal(err)
+	}
+
+	if dimension.ColumnCount != 4{
+		t.Error("dimension column count should equal 4 but equals", dimension.ColumnCount)
+	}
+	if dimension.RowCount != 2 {
+		t.Error("dimension row count should equal 2 but equals", dimension.RowCount)
+	}
+
+	zeroDataRowDim, err := MakeTableDimension(f, zeroRowFourColumnName)
+	if err != nil{
+		t.Fatal(err)
+	}
+	if zeroDataRowDim.RowCount != 0{
+		t.Error("zero row dimension should have 0 rows but has", zeroDataRowDim.RowCount)
+	}
+	if zeroDataRowDim.ColumnCount != 4{
+		t.Error("zero row dimension should have 4 columns but has", zeroDataRowDim.ColumnCount)
+	}
+
+	_, blankErr := MakeTableDimension(f, blankName)
+	if blankErr != ErrNoHeaderRow{
+		t.Error("ErrNoHeader row should be returned when reading a blank tab")
+	}
+
+
+}
